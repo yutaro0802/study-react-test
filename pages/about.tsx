9ea6c925cf73d9a3e03f2d22af9a8b4ff3f69adc
Yutaro Hatanaka <yuu08002@gmail.com>
2025-10-02 18:32:20 +0900

    ✨ アバウトページを追加

---
+++ pages/about.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Head from "next/head"
+import { Footer } from "../components/Footer"
+import {Links} from "../components/Links"
+
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div
+      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
+    >
+      <Head>
+        <title>
+          about
+        </title>
+      </Head>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1>about page</h1>
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+              pages/index.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
+        <Links />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+
+
+
