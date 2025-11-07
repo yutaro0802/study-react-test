951a108c7057eb7063e1c18948115f74962e3051
Yutaro Hatanaka <yuu08002@gmail.com>
2025-11-07 18:41:05 +0900

    ♻️ linksのリファクタリングとcomponentの拡張子の変更

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Links } from "./Links";
 
+
+
 export function MainPage(props) {
   return (
     <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
