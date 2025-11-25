04b4715b6e0f9cf39409055fdb1d5bf2824094c8
Yutaro Hatanaka <yuu08002@gmail.com>
2025-11-25 09:55:56 +0900

    ⚗️

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
-import Head from "next/head";
+import Head from "next/head"
 import { Geist, Geist_Mono } from "next/font/google";
-import { Header } from "@/src/components/Header";
-import { Headline } from "src/components/Headline";
-import { MainPage } from "src/components/MainPage";
-import { Footer } from "@/src/components/Footer";
-
-// import { Footer } from "src/components/Footer";
+import { Footer } from "../components/Footer"
+import { Header } from "@/components/Header";
+import { Headline } from "../components/Headline";
+import { MainPage } from "../components/MainPage";
+import { useCallback } from "react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
-
 
 
 const geistMono = Geist_Mono({
@@ -19,17 +16,28 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
 export default function Home() {
+  const foo = 1
+
+  const handleClick = useCallback((e) => {
+          console.log(e.target.href);
+          e.preventDefault();
+          alert(foo)
+        }, []);
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
       <Head>
-        <title>index</title>
+        <title>
+          index
+        </title>
       </Head>
+      <a href="/about" onClick={handleClick}>ぼたん</a>
       <Header />
-      <Headline title="Index Page" page="index" />
-      <MainPage page="index" />
+      <Headline title="Index Page" page="index"/>
+      <MainPage page="index"/>
       <Footer />
     </div>
   );
