80940c7436ebb9b82dca5e27522362210d11acc5
Yutaro Hatanaka <yuu08002@gmail.com>
2025-12-25 14:40:08 +0900

    ♻️indexhtmlのリファクタリング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = useCallback((e) => {
+  const handleClick = useCallback(() => {
     if (count < 10) {
       setCount((count) => count + 1);
     }
